907a0be3f05aa9ec0fbf73d7faa0d62790898f13
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-12 22:37:33 +0100

    Initialisation (PHP: Connexion à la base de données)

---
+++ db/database.php
@@ -0,0 +1,16 @@
+
+
+<?php
+
+$servername = "localhost";
+$user = "root";
+$password = "37533753";
+$dbname = "africa";
+
+$connect = mysqli_connect($servername,$user,$password,$dbname);
+
+// if($connect){
+//     echo "data connect";
+//     exit();
+// }
+//     echo "error";
