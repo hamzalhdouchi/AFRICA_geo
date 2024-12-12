b9b08dc4518998395d30a25210acb8cc03eb3e43
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-12 22:49:49 +0100

    Configuration de la base de données et initialisation

--- views/ajouter.php
+++ views/ajouter.php
@@ -18,3 +18,13 @@ if (isset($_POST['supprimer'])) {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Africa Gio</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="overflow-x-hidden relative">
+    <div>
