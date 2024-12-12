c0cfe23cbfd39b430facaba8b860c263cf23d2df
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-12 22:41:21 +0100

    Barre de navigation (nav)

--- views/index.php
+++ views/index.php
@@ -7,3 +7,17 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="overflow-x-hidden">
+<nav class="border-gray-200 bg-green-600 block w-screen">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img class="w-12" src="../public/img/logo.png" alt="logo">
+            <span class="self-center text-2xl font-semibold text-white">Africa Gio</span>
+        </a>
+        <ul class="flex space-x-8">
+            <li><a href="../index.php" class="text-white hover:text-orange-700">Home</a></li>
+            <li><a href="./forms.html" class="text-white hover:text-orange-700">Ajouter</a></li>
+            <li><a href="#" class="text-white hover:text-orange-700">Pays</a></li>
+            <li><a href="#" class="text-white hover:text-orange-700">Ville</a></li>
+        </ul>
+    </div>
+</nav>
