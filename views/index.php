10281837435fb985af6cdc24881f96c3d35705bf
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-12 11:27:07 +0100

    create nav bar pour page index.php

--- views/index.php
+++ views/index.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+<div>
+        <nav class="border-gray-200 bg-green-600 block w-screen">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img class="w-12" src="./public/img/logo.png" alt="logo">
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Africa Gio</span>
+                </a>
+                <div class="hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+                    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-green-600">
+                        <li><a href="./index.php" class="block py-2 px-3 text-white hover:text-green-900">Home</a></li>
+                        <li><a href="./views/AjouterPayeVille.php" class="block py-2 px-3 text-white hover:text-green-900">Ajouter</a></li>
+                        <li><a href="#" class="block py-2 px-3 text-white hover:text-green-900">Payes</a></li>
+                        <li><a href="#" class="block py-2 px-3 text-white hover:text-green-900">Ville</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 </body>
 </html>
