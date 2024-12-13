ee2d75503b7932a771cacd167dadb51c908f1829
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-13 17:26:14 +0100

    Déclaration de la div pour l'écran de fond

--- views/villes.php
+++ views/villes.php
@@ -19,15 +19,7 @@ require '../db/database.php';
             <img class="w-12" src="../public/img/logo.png" alt="logo">
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Africa Gio</span>
         </a>
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-        <nav class=" border-gray-200 bg-green-600 block w-screen">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img class="w-12" src="../public/img/logo.png" alt="logo">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Africa Gio</span>
-        </a>
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search"
 <nav class=" border-gray-200 bg-green-600 block w-screen">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -82,6 +74,8 @@ if (isset($_POST['modifer'])) {
         description = '$Description',
         population = '$population'
         WHERE id = '$id'");
+    }
+}
 ?>
 
 <div id="alert-border-3" class="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800 absolute w-[50vw] top-20 rounded-lg" role="alert">
@@ -99,6 +93,24 @@ if (isset($_POST['modifer'])) {
     </button>
 </div>
 
+}else{
+?>
+    <div id="alert-border-2" class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800" role="alert">
+        <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <div class="ms-3 text-sm font-medium">
+            entre les donne correct.
+        </div>
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-border-2" aria-label="Close">
+            <span class="sr-only">Dismiss</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+        </button>
+    </div>
+<?php
+}
 
 
 
